730dc5c1687561b7eb7e57254197ea6825037cee
Davidchhing <davidhchhing@gmail.com>
2018-05-17 18:44:05 -0700

    added pictures of samoyed

--- JS/main.js
+++ JS/main.js
@@ -5,11 +5,11 @@ var projects = [];
 function Projects(rawDataObject) {
     this.completeDate = rawDataObject.completeDate;
     this.projectName = rawDataObject.projectName;
-    this.projectInfo = rawDataObject.projectInfo;
+    this.body = rawDataObject.projectInfo;
 }
 
 Projects.prototype.toHtml = function() {
-    var template = $('.project-template').html();
+    var template = $('#project-template').html();
     var compile = Handlebars.compile($template);
     $('main').append(compile(this));
     return template(this);
